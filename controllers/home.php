38748054d1c7424f67db8f3c4fc79d031cd79247
appadmindelhi <anand454pandey@gmail.com>
2017-07-04 15:46:02 +0530

    Update home.php
    Home page after user login successful

--- controllers/home.php
+++ controllers/home.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Home extends CI_Controller{
 
 	/**
@@ -15,7 +14,7 @@ class Home extends CI_Controller{
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
+	 * map to /index.php/Home/index
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
